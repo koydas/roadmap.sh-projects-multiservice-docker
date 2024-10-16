994440e53ba43efa23495a8c39a22a434efc195c
Stephane Hamel <koydas@hotmail.com>
2024-10-16 13:47:58 -0400

    api - hard coded users

--- services/api service/src/server.js
+++ services/api service/src/server.js
@@ -3,8 +3,29 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello World !")
+app.use(express.json())
+
+const users = [{
+    id: 1,
+    firstName: 'john',
+    lastName: 'doe'
+}]
+
+app.get("/users", (req, res) => {
+    res.send(users)
+})
+
+app.post("/users", (req, res) => {
+    const newId = users[users.length - 1].id + 1
+    const newUser = {
+        id: newId,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName
+    }
+
+    users.push(newUser)
+    
+    res.send(`New user added: ${newId} '${newUser.firstName} ${newUser.lastName}'`)
 })
 
 app.listen(port, (error) => {
