c5514519091bb754d57dec3d1142ed79903ba998
Stephane Hamel <koydas@hotmail.com>
2024-10-16 13:31:29 -0400

    adding mongodb dependancies

--- services/api service/src/server.js
+++ services/api service/src/server.js
@@ -3,10 +3,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.listen(port, (error) =>{
-    if(!error)
-        console.log("Server is Successfully Running, and App is listening on port "+ port)
-    else 
+app.get("/", (req, res) => {
+    res.send("Hello World !")
+})
+
+app.listen(port, (error) => {
+    if (!error)
+        console.log("Server is Successfully Running, and App is listening on port " + port)
+    else
         console.log("Error occurred, server can't start", error);
-    }
+}
 );
